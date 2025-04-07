f16c94780d7d9d9a7fe17acbfaa212a07f44eb99
default <lucas.nguyen@mths.ca>
2025-04-07 17:16:07 +0000

    work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Lucas Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Nguyen
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function enterClicked() {
+  // input
+  const farenheit = parseFloat(document.getElementById("farenheit").value)
+  // process
+  const celcius = (farenheit - 32) * 5 / 9
+  // output
+  document.getElementById("answer").innerHTML =
+    "The temperature in celcius is: " + celcius.toFixed(3) + " °C."
+}
